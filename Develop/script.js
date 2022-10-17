6fe0507d71ff5188aa888353e3035610fe264b94
devin <devinelliotgomez@gmail.com>
2022-10-17 16:13:12 -0500

    added four conformation prompts to determine if the user wanted numbers, symbols, uppercase, and lowercase in their passcode

--- Develop/script.js
+++ Develop/script.js
@@ -3,16 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
   var confirmGenerate = confirm ("Click OK to begin selecting password criteria.")
-  var generatorInput = prompt ("Thanks for using this password generator. Our first order of buisness is to decide how long you would like your password to be (between 8 and 128 characters).");
+  var generatorInput = prompt ("Thanks for using this password generator. What will be the integer length of your password? (between 8 and 128 characters).");
   var generatorLength = parseInt(generatorInput)
 
 if (generatorLength < 8) {
-  alert("That password will be too small unfortunately ")
+  alert("That password will be too small unfortunately.")
   }
 
 if (generatorLength > 128) {
-  alert("That password will be too big unfortunately")
+  alert("That password will be too big unfortunately.")
+
+var lowercaseConfirm = confirm("Are you ok with lowercase characters in your password?")
+var uppercaseConfirm = confirm("Are you ok with uppercase characters in your password?")
+var symbolsConfirm = confirm("Are you ok with symbols in your password?")
+var NumbersConfirm = confirm("Are you ok with Numbers in your password?")
+
+
 }}
+
+
 
 
 // Write password to the #password input
