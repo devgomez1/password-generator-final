079874c118cae3d4623b1dce06afac38b7829ffd
devin <devinelliotgomez@gmail.com>
2022-10-17 15:21:58 -0500

    noticed that the gereratePassword function did not exist, began working on that and added some variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  var generatorInput = prompt ("Hello, thanks for using this password generator. Our first order of buisness is to decide how long you would like your password to be (between 8 and 128 characters).");
+  var generatorLength = valueOf(generatorInput)
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
