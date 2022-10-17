42b2ec66d52480cc088e26a8eba5b94c79973537
devin <devinelliotgomez@gmail.com>
2022-10-17 15:56:04 -0500

    added in two variables, one to confirm they wanted to begin selecting criteria, and one to determine the interger value of the first prompt, how long they wanted their password to be, here is where I ran into a problem, first defining the variable as equal to the respective prompt.length, but instead it was reading the character length, not the actual interger value. This led me to google where i discovered the parseINT which is exaclty what I needed, it returns the string at an interger value, this let me set up less than or greater than statements to dececect if a password was too small.

--- Develop/script.js
+++ Develop/script.js
@@ -2,10 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
-  var generatorInput = prompt ("Hello, thanks for using this password generator. Our first order of buisness is to decide how long you would like your password to be (between 8 and 128 characters).");
-  var generatorLength = valueOf(generatorInput)
+  var confirmGenerate = confirm ("Click OK to begin selecting password criteria.")
+  var generatorInput = prompt ("Thanks for using this password generator. Our first order of buisness is to decide how long you would like your password to be (between 8 and 128 characters).");
+  var generatorLength = parseInt(generatorInput)
 
-}
+if (generatorLength < 8) {
+  alert("That password will be too small unfortunately ")
+  }
+
+if (generatorLength > 128) {
+  alert("That password will be too big unfortunately")
+}}
 
 
 // Write password to the #password input
